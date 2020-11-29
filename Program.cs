ee7f762d28ab81ab0c78db92fc317accb04025a3
bardin08 <asdasfb326@gmail.com>
2020-11-29 15:00:56 +0200

    Add configuration and service host

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace Bot
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((_, services) =>
+                {
+                    // ... Define services here ...
+                })
+                .Build();
+        }
+
+        private static void BuildConfig(ConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsetting.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
         }
     }
 }
