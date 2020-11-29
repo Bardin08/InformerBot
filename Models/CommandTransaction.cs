ebb8e033a20d1af8c7654757571560ef4e6a3b3b
bardin08 <asdasfb326@gmail.com>
2020-11-29 20:25:43 +0200

    Update command transaction model

--- Models/CommandTransaction.cs
+++ Models/CommandTransaction.cs
@@ -2,8 +2,11 @@
 {
     internal sealed class CommandTransaction : TransactionBase
     {
-        public CommandTransaction(int recepientId) : base(recepientId)
+        public CommandTransaction(int recepientId, string command) : base(recepientId)
         {
+            Command = command;
         }
+
+        public string Command { get; set; }
     }
 }
