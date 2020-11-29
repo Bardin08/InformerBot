3bdc53b6222b583232800f7f2a037b2b27893f12
bardin08 <asdasfb326@gmail.com>
2020-11-29 17:36:45 +0200

    Add informer bot command interface

---
+++ Interfaces/IInformerBotCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace Bot.Interfaces
+{
+    public interface IInformerBotCommand
+    {
+        public string Name { get; }
+
+        Task ExecuteAsync(object transaction, ITelegramBotClient botClient);
+    }
+}
