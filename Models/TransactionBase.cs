b76980ef0a345fd603241fe042ffad8b5ee8ea4b
bardin08 <asdasfb326@gmail.com>
2020-11-29 17:35:36 +0200

    Add transaction base model

---
+++ Models/TransactionBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Bot.Models
+{
+    internal class TransactionBase
+    {
+        public TransactionBase(int recepientId)
+        {
+            IsComplete = false;
+            TransactionId = RecepientId = recepientId;
+            MessageIds = new List<int>();
+        }
+
+        public bool IsComplete { get; set; }
+
+        public int RecepientId { get; set; }
+
+        public int TransactionId { get; set; }
+
+        public List<int> MessageIds { get; set; }
+    }
+}
