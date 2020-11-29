94c2c5ae5de06b121367781477c3b47fdc1c70d3
bardin08 <asdasfb326@gmail.com>
2020-11-29 20:26:49 +0200

    Add application database context

---
+++ Contexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Bot.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bot.Contexts
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
