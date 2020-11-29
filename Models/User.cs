d44fea67924fc772a603f94d1707659662dd04c1
bardin08 <asdasfb326@gmail.com>
2020-11-29 19:58:07 +0200

    Add user model

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bot.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string RealName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Username { get; set; }
+
+        public string LanguageCode { get; set; }
+    }
+}
