c711eb3480c1b19d9b09e9e9f16cbc649481be1d
bardin08 <asdasfb326@gmail.com>
2020-11-29 15:41:00 +0200

    Add informer bot client

---
+++ InformerBotClient.cs
@@ -0,0 +1,15 @@
+﻿using Bot.Interfaces;
+using Telegram.Bot;
+
+namespace Bot
+{
+    public class InformerBotClient : IInformerBotClient
+    {
+        public ITelegramBotClient BotClient { get; set; }
+
+        public InformerBotClient(ITelegramBotClient botClient)
+        {
+            BotClient = botClient;
+        }
+    }
+}
