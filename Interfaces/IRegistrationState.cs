37e545847a2ab61e2ba54e4f9e6b7e15708c33e5
bardin08 <asdasfb326@gmail.com>
2020-11-30 10:32:07 +0200

    Add registration state interface

---
+++ Interfaces/IRegistrationState.cs
@@ -0,0 +1,12 @@
+﻿using Bot.Contexts;
+using Bot.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Bot.Interfaces
+{
+    public interface IRegistrationState
+    {
+        internal void Update(Message message, RegistrationTransaction transaction, ApplicationDbContext dbContext, ITelegramBotClient botClient);
+    }
+}
