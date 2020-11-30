1ad1c4cd3b8c04f5fe5ae8827d972310b169d528
bardin08 <asdasfb326@gmail.com>
2020-11-30 14:46:37 +0200

    Add command for post creation

---
+++ Commands/CreatePostCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Bot.Contexts;
+using Bot.Interfaces;
+using Bot.Models;
+using Telegram.Bot;
+
+namespace Bot.Commands
+{
+    internal sealed class CreatePostCommand : IInformerBotCommand
+    {
+        public string Name => "/createpost";
+
+        internal delegate void PostCreatingEvent(PostCreationTransaction transaction);
+        internal static event PostCreatingEvent TransactionInitiated;
+
+        public Task ExecuteAsync(object transaction, ApplicationDbContext db, ITelegramBotClient botClient)
+        {
+            (transaction as TransactionBase).IsComplete = true;
+            TransactionInitiated?.Invoke(new PostCreationTransaction((transaction as TransactionBase).RecepientId)
+            {
+                TransactionType = "postcreating",
+                PostCreatingState = new Behaviour.PostCreatingStates.PostCreatingBegin()
+            });
+
+            return Task.CompletedTask;
+        }
+    }
+}
