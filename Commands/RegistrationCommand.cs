20ead9b522fbe00367ea27628f7b3cbbef306608
bardin08 <asdasfb326@gmail.com>
2020-11-30 10:31:20 +0200

    Add registration command

---
+++ Commands/RegistrationCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Bot.Behaviour.RegistrationStates;
+using Bot.Contexts;
+using Bot.Interfaces;
+using Bot.Models;
+using Telegram.Bot;
+
+namespace Bot.Commands
+{
+    public sealed class RegistrationCommand : IInformerBotCommand
+    {
+        internal delegate void UserRegistrationEvent(RegistrationTransaction transaction);
+        internal static event UserRegistrationEvent TransactionInitiated;
+
+        public string Name => "/register";
+
+        public Task ExecuteAsync(object transaction, ApplicationDbContext db, ITelegramBotClient botClient)
+        {
+            (transaction as TransactionBase).IsComplete = true;
+            TransactionInitiated?.Invoke(new RegistrationTransaction((transaction as CommandTransaction).RecepientId) { RegistationState = new RegistrationTransactionCreate() });
+
+            return Task.CompletedTask;
+        }
+    }
+}
