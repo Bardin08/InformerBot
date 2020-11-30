37f171957925f97aa6de0b0c282a1c4bc71c0a82
bardin08 <asdasfb326@gmail.com>
2020-11-30 14:42:44 +0200

    Add post creation transaction models

---
+++ Models/PostCreationTransaction.cs
@@ -0,0 +1,15 @@
+﻿using Bot.Interfaces;
+
+namespace Bot.Models
+{
+    internal sealed class PostCreationTransaction : TransactionBase
+    {
+        public PostCreationTransaction(int recepientId) : base(recepientId)
+        {
+        }
+
+        public string PostText { get; set; }
+
+        public IPostCreatingState PostCreatingState { get; set; }
+    }
+}
