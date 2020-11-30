2bbffab9a0d26bab9f4aba0438396b7c59921088
bardin08 <asdasfb326@gmail.com>
2020-11-30 14:45:43 +0200

    Add post creation interface

---
+++ Interfaces/IPostCreatingState.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Bot.Contexts;
+using Bot.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Bot.Interfaces
+{
+    internal interface IPostCreatingState
+    {
+        Task Update(Message message, PostCreationTransaction transaction, ApplicationDbContext dbContext, ITelegramBotClient botClient);
+    }
+}
