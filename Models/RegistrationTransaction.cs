9caa1b647da9783762380ad7a4b3c71b7a470116
bardin08 <asdasfb326@gmail.com>
2020-11-30 10:30:51 +0200

    Add registration transaction model

---
+++ Models/RegistrationTransaction.cs
@@ -0,0 +1,14 @@
+﻿using Bot.Interfaces;
+
+namespace Bot.Models
+{
+    internal sealed class RegistrationTransaction : TransactionBase
+    {
+        public RegistrationTransaction(int recepientId) : base(recepientId)
+        {
+            TransactionType = "registration";
+        }
+
+        public IRegistrationState RegistationState { get; set; }
+    }
+}
